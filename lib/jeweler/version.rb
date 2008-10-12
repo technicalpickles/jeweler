cb26038354dfacf85bfd56489eebd46f046c8b51
Josh Nichols <josh@technicalpickles.com>
2008-10-12 12:52:10 -0400

    Version bump.

--- lib/jeweler/version.rb
+++ lib/jeweler/version.rb
@@ -1,7 +1,7 @@
-class Jeweler
-  module Version
-    MAJOR = 0
-    MINOR = 0
-    PATCH = 5
+  class Jeweler
+    module Version
+      MAJOR = 0
+      MINOR = 0
+      PATCH = 6
+    end
   end
-end
