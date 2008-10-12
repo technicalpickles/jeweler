6463c38ac17d1bc5e494dcdbe3f3e55ba9620f95
Josh Nichols <josh@technicalpickles.com>
2008-10-12 03:01:08 -0400

    Moved some more into versioning.

--- lib/jeweler/versioning.rb
+++ lib/jeweler/versioning.rb
@@ -15,5 +15,32 @@ class Jeweler
     def version
       "#{major_version}.#{minor_version}.#{patch_version}"
     end
+    
+  protected
+    def version_module_path
+      File.join(@base_dir, 'lib', @gemspec.name, 'version.rb')
+    end
+  
+    def version_module
+      constantize("#{main_module_name}::Version")
+    end
+  
+    def refresh_version
+      # Remove the constants, so we can reload the version_module
+      undefine_versions()
+      load_version()
+    end
+
+    def undefine_versions
+      version_module.module_eval do
+        remove_const(:MAJOR) if const_defined?(:MAJOR)
+        remove_const(:MINOR) if const_defined?(:MINOR)
+        remove_const(:PATCH) if const_defined?(:PATCH)
+      end
+    end
+
+    def load_version
+      load version_module_path
+    end
   end
 end
