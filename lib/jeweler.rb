7ed3ed3ca4299f02c07c7fc3d02fed1bb12ccbef
Josh Nichols <josh@technicalpickles.com>
2008-12-23 10:33:40 -0500

    Moved methods out of Jeweler::Gemspec into Jeweler itself.

--- lib/jeweler.rb
+++ lib/jeweler.rb
@@ -13,7 +13,6 @@ require 'jeweler/tasks'
 class Jeweler
   include Jeweler::Bumping
   include Jeweler::Versioning
-  include Jeweler::Gemspec
   include Jeweler::Release
 
   attr_reader :gemspec
@@ -29,6 +28,54 @@ class Jeweler
     if File.exists?(File.join(base_dir, '.git'))
       @repo = Git.open(base_dir)
     end
+  end
+
+  # Writes out the gemspec
+  def write_gemspec
+    self.refresh_version
+
+    helper = gemspec_helper do |s|
+      s.version = self.version
+      s.date = Time.now
+    end
+
+    helper.write
+
+    puts "Generated: #{helper.path}"
+  end
+
+  # Validates the project's gemspec from disk in an environment similar to how 
+  # GitHub would build from it. See http://gist.github.com/16215
+  def validate_gemspec
+    begin
+      gemspec_helper.parse
+      puts "#{gemspec_path} is valid."
+    rescue Exception => e
+      puts "#{gemspec_path} is invalid. See the backtrace for more details."
+      raise
+    end
+  end
+
+
+  # is the project's gemspec from disk valid?
+  def valid_gemspec?
+    gemspec_helper.valid?
+  end
+
+  # parses the project's gemspec from disk without extra sanity checks
+  def unsafe_parse_gemspec(data = nil)
+    data ||= File.read(gemspec_path)
+    eval(data, binding, gemspec_path)
+  end
+
+  protected
+
+  def gemspec_helper(&block)
+    GemSpecHelper.new(@gemspec, @base_dir, &block)
+  end
+
+  def gemspec_path
+    gemspec_helper.path
   end
 end
 
--- lib/jeweler/gemspec.rb
+++ lib/jeweler/gemspec.rb
@@ -1,4 +1,5 @@
 class Jeweler
+
   class GemSpecHelper
 
     attr_accessor :spec, :base_dir
@@ -36,52 +37,5 @@ class Jeweler
       Thread.new { eval("$SAFE = 3\n#{data}", binding, path) }.join 
     end
 
-  end
-
-  module Gemspec
-    # Writes out the gemspec
-    def write_gemspec
-      self.refresh_version
-
-      helper = gemspec_helper do |s|
-        s.version = self.version
-        s.date = Time.now
-      end
-      
-      helper.write
-
-      puts "Generated: #{helper.path}"
-    end
-
-    # Validates the gemspec in an environment similar to how GitHub would build
-    # it. See http://gist.github.com/16215
-    def validate_gemspec
-      begin
-        gemspec_helper.parse
-        puts "#{gemspec_path} is valid."
-      rescue Exception => e
-        puts "#{gemspec_path} is invalid. See the backtrace for more details."
-        raise
-      end
-    end
-
-
-    def valid_gemspec?
-      gemspec_helper.valid?
-    end
-
-    def unsafe_parse_gemspec(data = nil)
-      data ||= File.read(gemspec_path)
-      eval(data, binding, gemspec_path)
-    end
-
-  protected
-    def gemspec_helper(&block)
-      GemSpecHelper.new(@gemspec, @base_dir, &block)
-    end
-
-    def gemspec_path
-      gemspec_helper.path
-    end
   end
 end
