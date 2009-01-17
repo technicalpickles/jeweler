a520b4928687ff25e147bed1ff7ac432c865456d
Josh Nichols <josh@technicalpickles.com>
2009-01-16 21:02:18 -0500

    Update the , and require the base class in test/spec helper.

--- lib/jeweler/templates/bacon/spec_helper.rb
+++ lib/jeweler/templates/bacon/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 require 'bacon'
 
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require '<%= file_name_prefix %>'
+
 # get a summary of errors raised and such
 Bacon.summary_on_exit
