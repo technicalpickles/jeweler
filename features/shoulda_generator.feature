a68604c9668f7d6cb0e0b5d3fbbae84b0b42caa8
Josh Nichols <josh@technicalpickles.com>
2009-01-31 23:56:28 -0500

    Even moar generator features.

--- features/shoulda_generator.feature
+++ features/shoulda_generator.feature
@@ -5,9 +5,13 @@ Feature: shoulda generator
 
   Scenario: generating a gem with shoulda tests
     Given a working directory
-    And working git configuration
-    And intentions to make a gem being tested by shoulda
+    And I configure my email address as 'bar@example.com'
+    And I configure my name as 'foo'
+    And I configure my github username as 'technicalpickles'
+    And I configure my github token as 'zomgtoken'
     And I decide to call the project 'the-perfect-gem'
+    And I decide to summarize the project as 'zomg, so good'
+    And intentions to make a gem being tested by shoulda
 
     When I generate a project
 
@@ -24,3 +28,15 @@ Feature: shoulda generator
 
     And 'coverage' is ignored by git
     And '*.sw?' is ignored by git
+    And '.DS_Store' is ignored by git
+
+    And Rakefile has 'the-perfect-gem' as the gem name
+    And Rakefile has 'bar@example.com' as the gem email
+    And Rakefile has 'zomg, so good' as the gem summary
+    And Rakefile has 'http://github.com/technicalpickles/the-perfect-gem' as the gem homepage
+    And Rakefile has 'test/**/*_test.rb' in the Rake::TestTask pattern
+    And Rakefile has 'test/**/*_test.rb' in the Rcov::RcovTask test_pattern
+    And Rakefile has 'test' in the Rcov::RcovTask libs
+
+    And LICENSE has the copyright as belonging to 'foo'
+    And LICENSE has the copyright as being in 2008
--- features/steps/generator_steps.rb
+++ features/steps/generator_steps.rb
@@ -12,13 +12,41 @@ Given /^I decide to call the project '((?:\w|-|_)+)'$/ do |name|
   @name = name
 end
 
+Given /I decide to summarize the project as '(.*)'/ do |summary|
+  @summary = summary
+end
+
+Given /^I configure my email address as '(.*)'$/ do |user_email|
+  @user_email = user_email
+end
+
+Given /^I configure my name as '(.*)'$/ do |user_name|
+  @user_name = user_name
+end
+
+Given /^I configure my github username as '(.*)'$/ do |github_user|
+  @github_user = github_user
+end
+
+Given /^I configure my github token as '(.*)'$/ do |github_token|
+  @github_token = github_token
+end
+
+
 Given /^working git configuration$/ do
-  Jeweler::Generator.any_instance.stubs(:read_git_config).
-        returns({'user.name' => 'foo', 'user.email' => 'bar@example.com', 'github.user' => 'technicalpickles', 'github.token' => 'zomgtoken'})
 end
 
 When /^I generate a project$/ do
-  @generator = Jeweler::Generator.new(@name, :directory => "#{@working_dir}/#{@name}")
+  Jeweler::Generator.any_instance.stubs(:read_git_config).
+        returns({
+          'user.name' => @user_name,
+          'user.email' => @user_email,
+          'github.user' => @github_user,
+          'github.token' => @github_token})
+
+  @generator = Jeweler::Generator.new(@name, 
+                                      :directory => "#{@working_dir}/#{@name}",
+                                      :summary => @summary)
 
   @stdout = OutputCatcher.catch_out do
     @generator.run
@@ -39,11 +67,67 @@ Then /^a file named '(.*)' is created$/ do |file|
   assert File.file?(file), "#{file} is not a file"
 end
 
-Then /^'(.*)' is ignored by git/ do |git_ignore|
-  @content ||= File.read((File.join(@working_dir, @name, '.gitignore')))
+Then /^'(.*)' is ignored by git$/ do |git_ignore|
+  @gitignore_content ||= File.read(File.join(@working_dir, @name, '.gitignore'))
 
-  assert_match git_ignore, @content
+  assert_match git_ignore, @gitignore_content
 end
+
+Then /^Rakefile has '(.*)' as the gem name$/ do |name|
+  @rakefile_content ||= File.read(File.join(@working_dir, @name, 'Rakefile'))
+
+  assert_match %Q{s.name = "#{name}"}, @rakefile_content
+end
+
+Then /^Rakefile has '(.*)' as the gem email$/ do |email|
+  @rakefile_content ||= File.read(File.join(@working_dir, @name, 'Rakefile'))
+  assert_match %Q{s.email = "#{email}"}, @rakefile_content
+end
+
+Then /^Rakefile has '(.*)' as the gem summary$/ do |summary|
+  @rakefile_content ||= File.read(File.join(@working_dir, @name, 'Rakefile'))
+  assert_match %Q{s.summary = %Q{#{summary}}}, @rakefile_content
+end
+
+Then /^Rakefile has '(.*)' as the gem homepage$/ do |homepage|
+  @rakefile_content ||= File.read(File.join(@working_dir, @name, 'Rakefile'))
+  assert_match %Q{s.homepage = "#{homepage}"}, @rakefile_content
+end
+
+Then /^Rakefile has '(.*)' in the Rake::TestTask pattern$/ do |pattern|
+  @rakefile_content ||= File.read(File.join(@working_dir, @name, 'Rakefile'))
+
+  assert_match %Q{t.pattern = '#{pattern}'}, @rakefile_content
+end
+
+Then /^Rakefile has '(.*)' in the Rcov::RcovTask test_pattern$/ do |test_pattern|
+
+  @rakefile_content ||= File.read(File.join(@working_dir, @name, 'Rakefile'))
+
+  assert_match "t.test_files = FileList['#{test_pattern}']", @rakefile_content
+
+end
+
+Then /^Rakefile has '(.*)' in the Rcov::RcovTask libs$/ do |libs|
+
+  @rakefile_content ||= File.read(File.join(@working_dir, @name, 'Rakefile'))
+
+  assert_match "t.libs << '#{libs}'", @rakefile_content
+
+end
+
+Then /^LICENSE has the copyright as belonging to '(.*)'$/ do |copyright_holder|
+  @license_content ||= File.read(File.join(@working_dir, @name, 'LICENSE'))
+
+  assert_match copyright_holder, @license_content
+end
+
+Then /^LICENSE has the copyright as being in (\d{4})$/ do |year|
+  @license_content ||= File.read(File.join(@working_dir, @name, 'LICENSE'))
+
+  assert_match year, @license_content
+end
+
 
 After do
   FileUtils.rm_rf @working_dir if @working_dir
