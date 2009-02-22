5e5be51f73a11aeb20525c375149b1dfd1fd69ce
Josh Nichols <josh@technicalpickles.com>
2009-02-22 01:30:38 -0500

    Comment/whitespace cleanup.

--- lib/jeweler/templates/bacon/helper.rb
+++ lib/jeweler/templates/bacon/helper.rb
@@ -5,5 +5,4 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require '<%= file_name_prefix %>'
 
-# get a summary of errors raised and such
 Bacon.summary_on_exit
--- lib/jeweler/templates/rspec/helper.rb
+++ lib/jeweler/templates/rspec/helper.rb
@@ -2,7 +2,6 @@ require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
 require '<%= file_name_prefix %>'
 
 Spec::Runner.configure do |config|
