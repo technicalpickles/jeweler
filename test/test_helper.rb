2ac9ec5c739e61343c704a27b57bdb0be0ee2f5e
Eloy Duran <eloy.de.enige@gmail.com>
2009-03-09 11:17:32 +0100

    Add gems in vendor/gems to the load path _before_ requiring any libs.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
+# Use vendored gem because of limited gem availability on runcoderun
+# This is loosely based on 'vendor everything'.
+Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', '**')].each do |dir|
+  lib = "#{dir}/lib"
+  $LOAD_PATH.unshift(lib) if File.directory?(lib)
+end
+
 require 'test/unit'
 
 require 'rubygems'
@@ -14,13 +21,6 @@ require 'jeweler'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'shoulda_macros/jeweler_macros'
-
-# Use vendored gem because of limited gem availability on runcoderun
-# This is loosely based on 'vendor everything'.
-Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', '**')].each do |dir|
-  lib = "#{dir}/lib"
-  $LOAD_PATH.unshift(lib) if File.directory?(lib)
-end
 
 class RubyForgeStub
   attr_accessor :userconfig, :autoconfig
