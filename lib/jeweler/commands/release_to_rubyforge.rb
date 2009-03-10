90773a249819ddf848c5fe9bb41052b391e36b3c
Josh Nichols <josh@technicalpickles.com>
2009-03-09 20:14:39 -0400

    Tweaked output.

--- lib/jeweler/commands/release_to_rubyforge.rb
+++ lib/jeweler/commands/release_to_rubyforge.rb
@@ -13,13 +13,13 @@ class Jeweler
         raise "rubyforge_project not configured.  Add this to the Jeweler::Tasks block in your Rakefile." if @gemspec.rubyforge_project.nil?
         
         @ruby_forge.configure rescue nil
-        output.puts 'Logging in'
+        output.puts 'Logging in rubyforge'
         @ruby_forge.login
 
         @ruby_forge.userconfig['release_notes'] = @gemspec.description if @gemspec.description
         @ruby_forge.userconfig['preformatted'] = true
 
-        output.puts "Releasing #{@gemspec.name} v. #{@version} as #{@gemspec.rubyforge_project}"
+        output.puts "Releasing #{@gemspec.name}-#{@version} to #{@gemspec.rubyforge_project}"
         @ruby_forge.add_release(@gemspec.rubyforge_project, @gemspec.name, @version.to_s, @gemspec_helper.gem_path)
       end
       
