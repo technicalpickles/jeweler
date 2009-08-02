4ff99c3009d0369db325466782125a7a8b8d9bf3
Joshua Nichols <josh@technicalpickles.com>
2009-08-02 15:48:09 -0400

    Removed checks for defined tasks.

--- test/test_tasks.rb
+++ test/test_tasks.rb
@@ -6,9 +6,11 @@ class TestTasks < Test::Unit::TestCase
 
   context 'instantiating Jeweler::Tasks' do
     setup do
-      Task.clear
-
       @jt = Jeweler::Tasks.new {}
+    end
+
+    teardown do
+      Task.clear
     end
 
     should 'assign @gemspec' do
@@ -28,24 +30,6 @@ class TestTasks < Test::Unit::TestCase
       Jeweler::Tasks.new do |s|
         assert !s.files.empty?
       end
-    end
-
-    should 'define tasks' do
-      assert Task.task_defined?(:build)
-      assert Task.task_defined?(:install)
-      assert Task.task_defined?(:gemspec)
-      assert Task.task_defined?(:build)
-      assert Task.task_defined?(:install)
-      assert Task.task_defined?(:'gemspec:validate')
-      assert Task.task_defined?(:'gemspec:generate')
-      assert Task.task_defined?(:version)
-      assert Task.task_defined?(:'version:write')
-      assert Task.task_defined?(:'version:bump:major')
-      assert Task.task_defined?(:'version:bump:minor')
-      assert Task.task_defined?(:'version:bump:patch')
-      assert Task.task_defined?(:'release')
-      assert Task.task_defined?(:'rubyforge:release:gem')
-      assert Task.task_defined?(:'rubyforge:setup')
     end
   end
 end
