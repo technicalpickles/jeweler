6b401e93129ebcecfd65662d679a2fd611ffa500
Joshua Nichols <josh@technicalpickles.com>
2009-10-08 00:48:46 -0400

    More thorough task tests.

--- test/jeweler/test_tasks.rb
+++ test/jeweler/test_tasks.rb
@@ -6,7 +6,8 @@ class TestTasks < Test::Unit::TestCase
 
   context 'instantiating Jeweler::Tasks' do
     setup do
-      @jt = Jeweler::Tasks.new {}
+      @gemspec_building_block = lambda {}
+      @tasks = Jeweler::Tasks.new &@gemspec_building_block
     end
 
     teardown do
@@ -14,23 +15,36 @@ class TestTasks < Test::Unit::TestCase
     end
 
     should 'assign @gemspec' do
-      assert_not_nil @jt.gemspec
+      assert_not_nil @tasks.gemspec
     end
 
-    should 'assign @jeweler' do
-      assert_not_nil @jt.jeweler
+    should 'not eagerly initialize Jeweler' do
+      assert ! @tasks.instance_variable_defined?(:@jeweler)
+    end
+
+    should 'set self as the application-wide jeweler tasks' do
+      assert_same @tasks, Rake.application.jeweler_tasks
+    end
+
+    should 'save gemspec building block for later' do
+      assert_same @gemspec_building_block, @tasks.gemspec_building_block
+    end
+
+    context 'Jeweler instance' do
+      setup do
+        @tasks.jeweler
+      end
+
+      should 'initailize Jeweler' do
+        assert @tasks.instance_variable_defined?(:@jeweler)
+      end
     end
 
     should 'yield the gemspec instance' do
       spec = nil
-      @jt = Jeweler::Tasks.new { |s| spec = s }
-      assert_not_nil @jt.jeweler.gemspec
+      @tasks = Jeweler::Tasks.new { |s| spec = s }
+      assert_not_nil @tasks.jeweler.gemspec
     end
 
-    should 'set the gemspec defaults before yielding it' do
-      Jeweler::Tasks.new do |s|
-        assert !s.files.empty?
-      end
-    end
   end
 end
