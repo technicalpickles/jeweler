bae6f6c82000d71c0a37f268fb4b3e538f7caf0b
Joshua Nichols <josh@technicalpickles.com>
2009-11-18 01:05:41 -0500

    Now with less sudo.

--- lib/jeweler/commands/install_gem.rb
+++ lib/jeweler/commands/install_gem.rb
@@ -1,5 +1,3 @@
-require 'rbconfig'
-
 class Jeweler
   module Commands
     class InstallGem
@@ -14,19 +12,7 @@ class Jeweler
         command = "#{gem_command} install #{gemspec_helper.gem_path}"
         output.puts "Executing #{command.inspect}:"
 
-        sh sudo_wrapper(command) # TODO where does sh actually come from!? - rake, apparently
-      end
-
-      def sudo_wrapper(command)
-        use_sudo? ? "sudo #{command}" : command
-      end
-
-      def use_sudo?
-        host_os !~ /mswin|windows|cygwin/i
-      end
-
-      def host_os
-        Config::CONFIG['host_os']
+        sh command # TODO where does sh actually come from!? - rake, apparently
       end
 
       def gem_command
