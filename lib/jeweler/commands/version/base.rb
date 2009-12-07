1ccd1faff405b0e7675508d23893d8a166a6044a
devrandom <c1.github@niftybox.net>
2009-12-06 16:52:55 -0800

    make versioning work when gem directory is not at top level

--- lib/jeweler/commands/version/base.rb
+++ lib/jeweler/commands/version/base.rb
@@ -1,9 +1,11 @@
+require 'pathname'
+
 class Jeweler
   module Commands
     module Version
       class Base
 
-        attr_accessor :repo, :version_helper, :gemspec, :commit
+        attr_accessor :repo, :version_helper, :gemspec, :commit, :base_dir
 
         def run
           update_version
@@ -20,9 +22,20 @@ class Jeweler
 
         def commit_version
           if self.repo
-            self.repo.add(version_helper.path)
+            self.repo.add(working_subdir.join(version_helper.path))
             self.repo.commit("Version bump to #{self.version_helper.to_s}")
           end
+        end
+
+        def working_subdir
+          return @working_subdir if @working_subdir
+          cwd = base_dir_path
+          @working_subdir = cwd.relative_path_from(Pathname.new(repo.dir.path))
+          @working_subdir
+        end
+
+        def base_dir_path
+          Pathname.new(base_dir).realpath
         end
 
 
@@ -32,6 +45,7 @@ class Jeweler
           command.version_helper = jeweler.version_helper
           command.gemspec = jeweler.gemspec
           command.commit = jeweler.commit
+          command.base_dir = jeweler.base_dir
 
           command
         end
