41a34a27ca056622c0a21cb0c4c6a710ff7f95a6
Greg Sterndale <gsterndale@gmail.com>
2010-04-08 23:42:22 -0400

    RSpec's spec_helper requires all files in the spec/support directory and subdirectories

--- lib/jeweler/templates/rspec/helper.rb
+++ lib/jeweler/templates/rspec/helper.rb
@@ -4,6 +4,9 @@ require '<%= require_name %>'
 require 'spec'
 require 'spec/autorun'
 
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 Spec::Runner.configure do |config|
   
