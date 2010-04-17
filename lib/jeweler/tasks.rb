284dd195300cc1435d23fb9bbf321f48e4cfd449
postmodern <postmodern.mod3@gmail.com>
2010-04-17 02:56:28 -0700

    Oops, you cannot do double splats in Ruby 1.8.x.

--- lib/jeweler/tasks.rb
+++ lib/jeweler/tasks.rb
@@ -193,14 +193,14 @@ class Jeweler
         dirs = ['lib']
         dirs.unshift('ext') if File.directory?('ext')
 
-        args = dirs.map { |dir| "-I#{dir}" }
         cmd = if File.exist?('Gemfile')
                 ['bundle', 'exec', 'irb']
               else
                 ['irb']
               end
+        cmd += dirs.map { |dir| "-I#{dir}" }
 
-        sh(*cmd, *args)
+        sh(*cmd)
       end
       
     end
