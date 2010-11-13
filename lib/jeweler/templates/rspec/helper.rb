ce87dae6fe3e3bce690e7b3344b6d815a6e670a2
Joshua Nichols <josh@technicalpickles.com>
2010-11-13 15:05:58 -0600

    Fix rspec helper to include lib and spec directory (thanks mtodd).

--- lib/jeweler/templates/rspec/helper.rb
+++ lib/jeweler/templates/rspec/helper.rb
@@ -1,5 +1,7 @@
-require '<%= require_name %>'
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
+require '<%= require_name %>'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
