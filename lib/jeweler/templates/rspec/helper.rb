688fc316ab445e7c560a4c71cce7a7e8f1199f26
Artem <artem@veremey.net>
2012-01-25 14:15:16 -0800

    Updating rspec helper to initialize simplecov if Ruby 1.9 is in use

--- lib/jeweler/templates/rspec/helper.rb
+++ lib/jeweler/templates/rspec/helper.rb
@@ -1,5 +1,26 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
+if RUBY_VERSION =~ /^1\.9/
+  require 'simplecov'
+
+  module SimpleCov::Configuration
+    def clean_filters
+      @filters = []
+    end
+  end
+
+  SimpleCov.configure do
+    clean_filters
+    load_adapter 'test_frameworks'
+  end
+
+  ENV["COVERAGE"] && SimpleCov.start do
+    add_filter "/.rvm/"
+  end
+end
+
+
 require 'rspec'
 require '<%= require_name %>'
 
