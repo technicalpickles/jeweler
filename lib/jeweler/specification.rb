8916f0be874522c1631b34330da1acf090f275fc
Julik Tarkhanov <me@julik.nl>
2016-10-10 17:19:13 +0200

    Update to the new Bundler dep listing

--- lib/jeweler/specification.rb
+++ lib/jeweler/specification.rb
@@ -70,10 +70,10 @@ class Jeweler
         if File.exist?('Gemfile')
           require 'bundler'
           bundler = Bundler.load
-          bundler.dependencies_for(:default, :runtime).each do |dependency|
+          bundler.require(:default, :runtime).each do |dependency|
             add_dependency dependency.name, *dependency.requirement.as_list
           end
-          bundler.dependencies_for(:development).each do |dependency|
+          bundler.require(:development).each do |dependency|
             add_development_dependency dependency.name, *dependency.requirement.as_list
           end
         end
